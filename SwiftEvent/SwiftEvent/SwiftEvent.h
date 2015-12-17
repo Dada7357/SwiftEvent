244550f5223968a95eb93c246aeea9eb1e408639
dada7357 <dada7357@hotmail.com>
2015-12-17 14:59:35 +0800

    test

---
+++ SwiftEvent/SwiftEvent/SwiftEvent.h
@@ -0,0 +1,19 @@
+//
+//  SwiftEvent.h
+//  SwiftEvent
+//
+//  Created by Dada on 15/12/17.
+//  Copyright © 2015年 Dada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftEvent.
+FOUNDATION_EXPORT double SwiftEventVersionNumber;
+
+//! Project version string for SwiftEvent.
+FOUNDATION_EXPORT const unsigned char SwiftEventVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftEvent/PublicHeader.h>
+
+
